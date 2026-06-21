236864411a8a96d5245d47d921e55febbb0fd9a6
NhatHao-cryto <22130073@st.hcmuaf.edu.vn>
2026-06-21 08:24:13 +0700

    User CRUD API

---
+++ backend/src/main/java/com/luxestay/homestay/dto/request/UserUpdateRequest.java
@@ -0,0 +1,60 @@
+package com.luxestay.homestay.dto.request;
+
+import java.time.LocalDate;
+
+public class UserUpdateRequest {
+    private String password;
+    private int phone;
+    private String email;
+    private String fullName;
+    private LocalDate dob;
+    private String sex;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ backend/src/main/java/com/luxestay/homestay/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.luxestay.homestay.repository;
+
+import com.luxestay.homestay.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
