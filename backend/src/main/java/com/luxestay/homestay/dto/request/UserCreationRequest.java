1ae60518ee67bfee910a1d5e9e545ac7505a712f
NhatHao-cryto <22130073@st.hcmuaf.edu.vn>
2026-06-21 12:34:05 +0700

    Exception handling and validation

--- backend/src/main/java/com/luxestay/homestay/dto/request/UserCreationRequest.java
+++ backend/src/main/java/com/luxestay/homestay/dto/request/UserCreationRequest.java
@@ -1,10 +1,15 @@
 package com.luxestay.homestay.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class UserCreationRequest {
 
+    @Size(min = 6, message = "Username must be at least 6 characters.")
     private String username;
+
+    @Size(min = 8, message = "Password must be at least 8 characters.")
     private String password;
     private int phone;
     private String email;
