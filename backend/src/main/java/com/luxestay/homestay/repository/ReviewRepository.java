f0464387d16d1c41593f6de6cdf32dad5d85c501
CAOSONHAO <107295048+CAOSONHAO@users.noreply.github.com>
2026-06-29 23:06:14 +0700

    Add files via upload

---
+++ backend/src/main/java/com/luxestay/homestay/repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.luxestay.homestay.repository;
+
+import com.luxestay.homestay.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, String> {
+    List<Review> findByHomestay_Id(Long homestayId);
+}
